d1d60257e860f565a3eb8d01dad54ce24e15c7da
teddybrian <tedb19@gmail.com>
2023-11-18 18:47:36 +0300

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -34,7 +34,11 @@ const IssueForm = ({ issue }: Props) => {
   const handleSubmitIssue = async (data: IssueFormData) => {
     setIsSubmitting(true);
     try {
-      await axios.post("/api/issues", data);
+      if (issue) {
+        await axios.patch(`/api/issues/${issue.id}`, data);
+      } else {
+        await axios.post("/api/issues", data);
+      }
       router.push("/issues");
     } catch (error) {
       setError("An error occured!");
